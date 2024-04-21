9e5c74700a500b94780b6eac6feaf08cfd0b0d9b
fadelalif <fadelkarut@gmail.com>
2024-04-21 14:12:24 +0700

    Perbaikan Finishing pada main

--- TaxCalculator/src/lib/TaxFunction.java
+++ TaxCalculator/src/lib/TaxFunction.java
@@ -18,36 +18,25 @@ public class TaxFunction {
 	 * ditambah sebesar Rp 4.500.000 per anak sampai anak ketiga.
 	 * 
 	 */
-		privat sta	privat
+    private static final int BASE_TAX_THRESHOLD = 54000000;
+    private static final int MARRIED_ADDITIONAL_THRESHOLD = 4500000;
+    private static final int CHILDREN_TAX_THRESHOLD = 1500000;
 
-	
-	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible,
-			boolean isMarried, int numberOfChildren) {
+    public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
+        if (numberOfMonthWorking > 12) {
+            System.err.println("More than 12 months working per year");
+        }
 
-		int tax = 0;
+        numberOfChildren = Math.min(numberOfChildren, 3);
 
-		if (numberOfMonthWorking > 12) {
-			System.err.println("More than 12 month working per year");
-		}
+        int taxableIncome = (monthlySalary + otherMonthlyIncome) * numberOfMonthWorking - deductible;
+        int tax;
+        if (isMarried) {
+            tax = (int) Math.round(0.05 * (taxableIncome - (BASE_TAX_THRESHOLD + numberOfChildren * CHILDREN_TAX_THRESHOLD + MARRIED_ADDITIONAL_THRESHOLD)));
+        } else {
+            tax = (int) Math.round(0.05 * (taxableIncome - BASE_TAX_THRESHOLD));
+        }
 
-		if (numberOfChildren > 3) {
-			numberOfChildren = 3;
-		}
-
-		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible
-					- (54000000 + 4500000 + (numberOfChildren * 1500000))));
-		} else {
-			tax = (int) Math.round(
-					0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
-		}
-
-		if (tax < 0) {
-			return 0;
-		} else {
-			return tax;
-		}
-
-	}
-
+        return Math.max(0, tax);
+    }
 }
